af902668052888e8df926398e3f595c605e7fac9
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-02 23:52:37 +0300

    make change header style in button click event

--- js/dom.js
+++ js/dom.js
@@ -1,10 +1,22 @@
 //TODO DOM
-const mainHeader = document.querySelector(".mainHeader");
-console.log(mainHeader);
-console.log(mainHeader.textContent);
+const ref = {
+  mainHeader: document.querySelector(".mainHeader"),
+  button: document.querySelector(".button"),
+};
 
-setTimeout(() => {
-  mainHeader.textContent = "Its change from JS";
-  mainHeader.style.color = "red";
-  mainHeader.style.backgroundColor = "black";
-}, 2000);
+ref.button.addEventListener("click", changeHeaderStyle);
+
+console.log(ref.mainHeader);
+console.log(ref.mainHeader.textContent);
+
+// setTimeout(() => {
+//   ref.mainHeader.textContent = "Its change from JS";
+//   ref.mainHeader.style.color = "red";
+//   ref.mainHeader.style.backgroundColor = "black";
+// }, 2000);
+
+function changeHeaderStyle() {
+  ref.mainHeader.textContent = "Its change from JS";
+  ref.mainHeader.style.color = "red";
+  ref.mainHeader.style.backgroundColor = "black";
+}
