c9a019ade4b1247274e30e21197a90ed6a58db12
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-05-13 19:20:11 +0300

    work with db

--- js/http.js
+++ js/http.js
@@ -1,10 +1,8 @@
 //* Делаем запрос на API пакемона
-
-const pokemonData = fetch("https://pokeapi.co/api/v2/pokemon/1").then(
-  (data) => {
-    return data.json();
-  }
-);
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/1";
+const pokemonData = fetch(BASE_URL).then((data) => {
+  return data.json();
+});
 
 pokemonData
   .then((pokemon) => {
